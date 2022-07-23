4e16e85668503ccd399a658a65ee3e6ec76e0d10
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2022-07-23 15:25:29 -0500

    Initial commit

---
+++ src/main/java/com/mdsperu/bigmacindex/BigMacIndexApplication.java
@@ -0,0 +1,13 @@
+package com.mdsperu.bigmacindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigMacIndexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigMacIndexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdsperu/bigmacindex/BigMacIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsperu.bigmacindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigMacIndexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
